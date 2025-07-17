653618d7c1614e8b461b01a08a4791310bba1949
Richard Wijaya Harianto <richardwijayaharianto@gmail.com>
2025-07-17 11:34:34 +0700

    first commit

---
+++ Fort/FortApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortApp.swift
+//  Fort
+//
+//  Created by Richard WIjaya Harianto on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
